2137a472bd5b8445b00c320adec3058d6c335366
Kim Igorevich <kim4001@bk.ru>
2022-02-19 21:05:06 +0300

    4 модуль

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,21 @@
 function highlight(table) {
-  // ваш код...
+    for (const row of table.tBodies[0].rows) {
+        if (row.cells[3].dataset.available == 'true') {
+            row.classList.add(`available`);
+        } else if (row.cells[3].dataset.available == 'false') {
+            row.classList.add(`unavailable`);
+        } else {
+            row.hidden = true;
+        }
+        if (row.cells[2].textContent == `m`) {
+            row.classList.add(`male`);
+        } else if (row.cells[2].textContent == `f`) {
+            row.classList.add(`female`);
+        }
+        if (Number(row.cells[1].textContent) < 18) {
+            row.style.textDecoration = `line-through`;
+        }
+    }
+
+
 }
